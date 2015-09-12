7b4b96991eedbbbf8e5522ae418ed589b580b7b9
yamasy1549 <sanae@yamasy.info>
2015-09-12 16:22:26 +0900

    Add btn helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def btn_edit(path)
+    link_to path, class: 'btn btn-xs' do
+      raw "<i class='fa fa-pencil'></i> Edit"
+    end
+  end
+
+  def btn_back(path)
+    link_to path, class: 'btn btn-xs' do
+      raw "<i class='fa fa-arrow-left'></i> Back"
+    end
+  end
 end
